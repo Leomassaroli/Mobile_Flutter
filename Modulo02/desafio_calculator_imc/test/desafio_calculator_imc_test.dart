2ae90ed10cab8ad184c761697ac86069bb603fba
LeoMassaroli <leomassaroli@gmail.com>
2024-12-04 09:25:31 -0300

    Upload e finalização do Desafio do Modulo02

--- Modulo02/desafio_calculator_imc/test/desafio_calculator_imc_test.dart
+++ Modulo02/desafio_calculator_imc/test/desafio_calculator_imc_test.dart
@@ -1,8 +1,40 @@
-import 'package:desafio_calculator_imc/desafio_calculator_imc.dart';
 import 'package:test/test.dart';
+import 'package:desafio_calculator_imc/utils.dart';
+
+import '../bin/desafio_calculator_imc.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  group('Testes de Cálculo IMC', () {
+    test('Calcula o IMC corretamente', () {
+      double peso = 86;
+      double altura = 1.83;
+      double imc = resultIMC(peso, altura);
+
+      expect(imc, closeTo(25.68, 0.01));
+    });
+
+    test('Verifica categoria IMC - Saudável', () {
+      double imc = 22.0;
+      String categoria = categoriaIMC(imc);
+
+      expect(categoria, equals("SAUDÁVEL"));
+    });
+
+    test('Verifica categoria IMC - Obesidade Grau I', () {
+      double imc = 32.0;
+      String categoria = categoriaIMC(imc);
+
+      expect(categoria, equals("OBESIDADE GRAU I"));
+    });
+
+    test('Verifica entrada de nome válido', () {
+      var nome = lerConsoleNome("Teste Nome");
+      expect(RegExp(r'^[a-zA-Z\s]+$').hasMatch(nome), isTrue);
+    });
+
+    test('Calcula IMC - valores negativos ou zero', () {
+      expect(() => resultIMC(-10, 1.75), throwsA(isA<Exception>()));
+      expect(() => resultIMC(60, 0), throwsA(isA<Exception>()));
+    });
   });
 }
